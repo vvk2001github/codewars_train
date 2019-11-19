eddccfd7ca7cd9ddfb04ed193ac7e7f381e23aaf
kulyaginv <kulyaginv@gmail.com>
2019-11-19 16:51:42 +0300

    rankingSystem/rankingSystem.py

---
+++ rankingSystem/rankingSystem.py
@@ -0,0 +1,12 @@
+class User:
+    def __init__(self):
+        self.rank = -8
+        self.progress = 0
+
+    def inc_progress(n):
+        pass
+
+if __name__ == "__main__":
+    user = User()
+    print(user.rank)
+    print(user.progress)
