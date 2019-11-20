b6bea6085c8aa4a9b205da99c20fa6f31e6fd7fd
kulyaginv <kulyaginv@gmail.com>
2019-11-20 12:18:31 +0300

    matrixDeterminant

--- rankingSystem/rankingSystem.py
+++ rankingSystem/rankingSystem.py
@@ -4,15 +4,24 @@ class User:
         self.progress = 0
 
     def inc_progress(self, n):
-        print('New rank: ', n)
+        print('New rank: ', n, '    Old rank: ', self.rank)
+        print('Old progr: ', self.progress)
+        if n >= 9 or n < -8 or n == 0:
+            raise
+
         if(self.rank == 8):
             return
 
-        if self.rank - 2 >= n:
-            return
+        #if self.rank - 2 >= n:
+        #    return
         #
         #!!!!!  dddddd
         #
+        d = n - self.rank
+        if  self.rank > 0 and n < 0:
+                d += 1
+        if d <= -2:
+            return
 
         if (n > self.rank): 
             d = n - self.rank
@@ -24,17 +33,21 @@ class User:
         if n == self.rank:
             self.progress += 3
 
+        if n < self.rank:
+            self.progress += 1
+
         while(self.progress >= 100):
             self.progress -= 100
             self.rank += 1
             if self.rank == 0:
                 self.rank += 1
-        while self.rank > 9:
-            self.rank -= 1
+        if self.rank >= 8:
+            self.rank = 8
+            self.progress = 0
 
 if __name__ == "__main__":
     user = User()
-    print(user.rank)
-    print(user.progress)
-    user.inc_progress(-8)
-    print(user.progress)
+    user.rank = 7
+    user.progress = 91
+    user.inc_progress(8)
+
